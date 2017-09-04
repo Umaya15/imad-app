fd1e1663fe8435c8b97ff4eb5bc8315df448f73d
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2017-09-05 00:19:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,55 +5,86 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne = {
-    title: 'Article One | Umaya',
-    heading: 'Article One',
-    date: 'Sep 4th, 2017',
-    content:`
-        <p>
-            This is a good start.
-        </p>
-        <p>
-            Liking what I'm doing.
-        </p>
-        <p>
-            May be will do well.
-        </p>
-};
+var articles = {
+        'articleOne':{
+        title: 'Article One | Umaya',
+        heading: 'Article One',
+        date: 'Sep 4th, 2017',
+        content:`
+            <p>
+                This is a good start.
+            </p>
+            <p>
+                Liking what I'm doing.
+            </p>
+            <p>
+                May be will do well.
+            </p>`
+        },
+        'articleTwo':{
+        title: 'Article Two | Umaya',
+        heading: 'Article Two',
+        date: 'Sep 5th, 2017',
+        content:`
+            <p>
+                This is a good start and going.
+            </p>
+            <p>
+                Liking what I'm doing for sure.
+            </p>
+            <p>
+                May be will do well too.
+            </p>`
+        },
+        'articleThree':{
+        title: 'Article Three | Umaya',
+        heading: 'Article Three',
+        date: 'Sep 6th, 2017',
+        content:`
+            <p>
+                This is a good start and going still.
+            </p>
+            <p>
+                Liking what I'm doing for sure and on.
+            </p>
+            <p>
+                May be will do well too and how.
+            </p>`
+        }
+    };
 
 function createTemplate(data){
     var title = data.title;
     var date = data.date;
     var heading = data.heading;
-    var content= = data.content;
-
-    var htmlTemplate = `
-    <html>
-    <head>
-        <title>
-            ${title}
-        </title>
-        <meta name="viewport" content="width=device-width, inital-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet"/>
-    </head>
-    <body>
-        <div class="container">
-            <div>
-                <o href="/">Home</o>
+    var content = data.content;
+    var htmlTemplate = 
+ `   <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, inital-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet"/>
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <o href="/">Home</o>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
             </div>
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>
-            <div>
-                ${date}
-            </div>
-            <div>
-                ${content}
-            </div>
-        </div>
-    </body>
-</html>
+        </body>
+    </html>
 `;
 return htmlTemplate;
 }
@@ -62,10 +93,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res){
-     res.send(createTemplate(articleOne));
+app.get('/:articleName', function(req, res){
+     res.send(createTemplate(articles[articleName]));
 });
-
+/*
 app.get('/article-two', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
@@ -73,7 +104,7 @@ app.get('/article-two', function(req, res){
 app.get('/article-three', function(req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-
+*/
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
