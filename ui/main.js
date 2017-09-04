acc90b6b2ed77480835ac6c07443e50fe279dfca
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2017-09-05 01:10:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML = "This is Amazing!";
 
 // move hasura img
 var img = document.getElementById('hasura-img');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function (){
+    var interval = setInterval(moveRight, 100);
   img.style.marginLeft = '100px';  
 };
