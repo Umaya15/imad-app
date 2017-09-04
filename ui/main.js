d81348bf76c57d5aa9b11346a4ca039d480119ee
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2017-09-05 01:04:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
 console.log('Loaded!');
+
+// change text content
 var element = document.getElementById("main-text");
 alert ("Pause");
 element.innerHTML = "This is Amazing!";
+
+// move hasura img
+var img = document.getElementByID("hasura-img");
+img.onclick = function (){
+  img.style.marginLeft = '100px';  
+};
