fc2a95331a9900917ab4dba9e0ff904a78d08fb5
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2017-09-05 23:22:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 
 //counter code
 var button = document.getElementById('counter');
+var counter = 0;
 button.onclick = function (){
     // Make a request to the counter endpoint
     
@@ -9,7 +10,6 @@ button.onclick = function (){
     
     //render the variable in the correct span
     counter = counter + 1;
-    console.log(counter.toString());
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 } ;
