34c88b25f90ee7530a0a9a94a955b7036db93cd3
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2017-09-06 00:27:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,20 +27,33 @@ request.send(null);
 
 //code for server request (AJAX)
 var nameInput = document.getElementById('name');
-var name = nameInput.value;
+var iname = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    //Make a request to the server and send the name
+     // Create a request object
+    var request = new XMLHttpRequest();
     
-    // capture a list of names and render it as a list
-    var names = ['name1', 'name2', 'name3'];
-    var list = '';
-    for (var i=0; i<names.length; i++){
-        list += '<li>' + names[i] + '</li>'
+    // capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            //True Action
+            if (request.status === 200){
+                var counter = request.responseText;
+                // capture a list of names and render it as a list
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i<names.length; i++){
+                    list += '<li>' + names[i] + '</li>'
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
     }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
-    };
+    request.open('GET','http://iravalvizhigal.imad.hasura-app.io/submit-name?name ='+ iname, true);
+    request.send(null);
+};
 
 /*
 // change text content
