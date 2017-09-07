f15521604deca4b13940f7cfd216c2ff96325967
Umaya15 <31180776+Umaya15@users.noreply.github.com>
2017-09-08 02:59:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,7 +155,7 @@ app.post('/login', function(req, res){
     var username = req.body.username;
     var password = req.body.password;
 
-    pool.query('SELECT * FROM "User" username = $1', [username], function (err, result){
+    pool.query('SELECT * FROM "User" WHERE username = $1', [username], function (err, result){
        if (err) {
            res.status(500).send(err.toString());
        } else {
